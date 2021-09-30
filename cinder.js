144663deb5cffa01c9bea32428d2ac71fbf76ac9
KerbalNo15 <70920991+KerbalNo15@users.noreply.github.com>
2021-09-29 21:38:08 -0800

    Add shorthand because I like shorthand

--- cinder.js
+++ cinder.js
@@ -1,5 +1,5 @@
 //Fibbonacci number generator. Change the first memory value to sequence index n-2
-let runThis = "19 0 1 2 load 0 1 - store 0 load 2 load 3 + del 2 notjump 4 0 1 print 3"
+let runThis = "19 0 1 2 l 0 1 - s 0 l 2 l 3 + d 2 !j 4 0 1 p 3"
 
 let program = [];
 let memory = [];
@@ -44,7 +44,7 @@ for(let i = 0; i < program.length; i++) {
     if(debug) console.log(b + " divided by " + a + " is " + (b/a+""))
   }
   //change value at stack index. This one is kind of weird. "A B X 0 store" would push X to A
-  if(program[i]=="store") {
+  if(program[i]=="store" || program[i]=="s") {
     let a = +program[i+1]
     if(debug) console.log("changing address " + a + " to " + memory[memory.length - 1])
     memory.splice(a, 1, memory[memory.length - 1])
@@ -52,28 +52,28 @@ for(let i = 0; i < program.length; i++) {
     i = i + 1
   }
   //load from stack index
-  if(program[i]=="load") {
+  if(program[i]=="load" || program[i]=="l") {
     let a = +program[i+1]
     i = i + 1
     memory.push(memory[a])
     if(debug) console.log("Loaded " + memory[a] + " from index " + a)
   }
   //delete stack item at index
-  if(program[i]=="del") {
+  if(program[i]=="del" || program[i]=="d") {
     let a = +program[i+1]
     i = i + 1
     memory.splice(a, 1)
     if(debug) console.log("deleted item at address " + a)
   }
   //print value at memory location
-  if(program[i] == "print") {
+  if(program[i] == "print" || program[i]=="p") {
     let a = +program[i+1]
     i = i + 1
     console.log(memory[a])
     if(debug) console.log("Printed value")
   }
   //jump to instruction number if the value at index is 0
-  if(program[i] == "jump") {
+  if(program[i] == "jump" || program[i]=="j") {
     let index = +program[i+1]
     let otherIndex = +program[i+2]
     let instruction = +program[i+3]
@@ -86,7 +86,7 @@ for(let i = 0; i < program.length; i++) {
   }
   }
   //jump to instruction number if the value at index is not 0
-  if(program[i] == "notjump") {
+  if(program[i] == "notjump" || program[i]=="!j") {
     let instruction = +program[i+1]
     let index = +program[i+2]
     let otherIndex = +program[i+3]
