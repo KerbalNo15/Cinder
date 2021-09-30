0f9146b90020a7253c5eb81fdb2dc42441e1458d
KerbalNo15 <70920991+KerbalNo15@users.noreply.github.com>
2021-09-29 21:31:57 -0800

    Couldn't stand RPN for the keywords so now they're backwards

--- cinder.js
+++ cinder.js
@@ -1,5 +1,5 @@
-//fibbonacci number generator
-let runThis = "19 0 1 2 0 load 1 - 0 store 2 load 3 load + 2 del 4 0 1 notjump 3 print"
+//Fibbonacci number generator. Change the first memory value to sequence index n-2
+let runThis = "19 0 1 2 load 0 1 - store 0 load 2 load 3 + del 2 notjump 4 0 1 print 3"
 
 let program = [];
 let memory = [];
@@ -45,55 +45,56 @@ for(let i = 0; i < program.length; i++) {
   }
   //change value at stack index. This one is kind of weird. "A B X 0 store" would push X to A
   if(program[i]=="store") {
-    let a = +memory[memory.length - 1]
-    if(debug) console.log("changing address " + a + " to " + memory[memory.length - 2])
-    memory.splice(a, 1, memory[memory.length - 2])
-    memory.splice(memory.length - 2, 2)
+    let a = +program[i+1]
+    if(debug) console.log("changing address " + a + " to " + memory[memory.length - 1])
+    memory.splice(a, 1, memory[memory.length - 1])
+    memory.splice(memory.length - 1, 1)
+    i = i + 1
   }
   //load from stack index
   if(program[i]=="load") {
-    let a = +memory[memory.length - 1]
-    memory.splice(memory.length - 1, 1)
+    let a = +program[i+1]
+    i = i + 1
     memory.push(memory[a])
     if(debug) console.log("Loaded " + memory[a] + " from index " + a)
   }
   //delete stack item at index
   if(program[i]=="del") {
-    let a = +memory[memory.length - 1]
-    memory.splice(memory.length - 1, 1)
+    let a = +program[i+1]
+    i = i + 1
     memory.splice(a, 1)
     if(debug) console.log("deleted item at address " + a)
   }
   //print value at memory location
   if(program[i] == "print") {
-    let a = +memory[memory.length - 1]
-    memory.splice(memory.length - 1, 1)
+    let a = +program[i+1]
+    i = i + 1
     console.log(memory[a])
     if(debug) console.log("Printed value")
   }
   //jump to instruction number if the value at index is 0
   if(program[i] == "jump") {
-    let index = +memory[memory.length - 1]
-    let otherIndex = +memory[memory.length - 2]
-    let instruction = +memory[memory.length - 3]
-    memory.splice(memory.length - 3, 3)
+    let index = +program[i+1]
+    let otherIndex = +program[i+2]
+    let instruction = +program[i+3]
     if(memory[index] == memory[otherIndex]){
       i = instruction - 1 //one less because i gets incremented at the end of the instruction loop
     if(debug) console.log("jumped to instruction at address " + instruction + ", which is " + program[instruction])
   } else {
+    i = i + 3
     if(debug) console.log("Did not jump")
   }
   }
   //jump to instruction number if the value at index is not 0
   if(program[i] == "notjump") {
-    let index = +memory[memory.length - 1]
-    let otherIndex = +memory[memory.length - 2]
-    let instruction = +memory[memory.length - 3]
-    memory.splice(memory.length - 3, 3)
+    let instruction = +program[i+1]
+    let index = +program[i+2]
+    let otherIndex = +program[i+3]
     if(memory[index] != memory[otherIndex]){
       i = instruction - 1 //one less because i gets incremented at the end of the instruction loop
       if(debug) console.log("jumped to instruction at address " + instruction + ", which is " + program[instruction])
     } else {
+      i = i + 3
       if(debug) console.log("Did not jump")
     }
   }
