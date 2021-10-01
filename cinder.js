653bb4d4977ee81c302b5b7076b337cabc11af0a
KerbalNo15 <70920991+KerbalNo15@users.noreply.github.com>
2021-10-01 12:46:25 -0800

    Better document how the interpreter works

--- cinder.js
+++ cinder.js
@@ -5,22 +5,29 @@ let program = [];
 let memory = [];
 let debug = false
 
-program = runThis.split(" ");
-for(let i = 0; i < program.length; i++) {
+program = runThis.split(" "); //split the input into instructions. Side effect of using whitespcae as a divisor is that strings and arrays cannot contain spaces.
+
+//The instuction loop. This is essentially on clock cycle per iteration.
+for(let i = 0; i < program.length; i++) { //iterate through the instruction loop
+
   //push numbers directly into the stack
   if(program[i].match("[0-9]") != null){
     memory.push(program[i])
   }
-  //ignore memory labels
+
+  //ignore memory labels. Memory labels are used to tell if statements where to jump to.
   else if(program[i].match(":[A-Z]")){}
+
   //push arbitrary strings directly to the stack with their quotation marks removed
   else if(program[i].match('"[^"]*"')) {
     memory.push(program[i].substr(1, program[i].length-2))
   }
+
   //push arrays directly to the stack with their brackets removed
   else if(program[i].match('\[[^"]*\]')) {
     memory.push(program[i].substr(1, program[i].length-2))
   }
+
   //addition
   if(program[i] == "+"){
     let a = +memory[memory.length - 1]
@@ -29,6 +36,7 @@ for(let i = 0; i < program.length; i++) {
     memory.push(b+a+"")
     if(debug) console.log(b + " plus " + a + " is " + (b+a+""))
   }
+
   //subtration
   if(program[i] == "-"){
     let a = +memory[memory.length - 1]
@@ -37,6 +45,7 @@ for(let i = 0; i < program.length; i++) {
     memory.push(b-a+"")
     if(debug) console.log(b + " minus " + a + " is " + (b-a+""))
   }
+
   //multiplication
   if(program[i] == "*"){
     let a = +memory[memory.length - 1]
@@ -45,6 +54,7 @@ for(let i = 0; i < program.length; i++) {
     memory.push(b*a+"")
     if(debug) console.log(b + " times " + a + " is " + (b*a+""))
   }
+
   //division
   if(program[i] == "/"){
     let a = +memory[memory.length - 1]
@@ -53,6 +63,7 @@ for(let i = 0; i < program.length; i++) {
     memory.push(b/a+"")
     if(debug) console.log(b + " divided by " + a + " is " + (b/a+""))
   }
+
   //modulo
   if(program[i] == "%"){
     let a = +memory[memory.length - 1]
@@ -61,7 +72,8 @@ for(let i = 0; i < program.length; i++) {
     memory.push(b%a+"")
     if(debug) console.log(b + " modulo " + a + " is " + (b%a+""))
   }
-  //change value at stack index. This one is kind of weird. "A 15 store A" would push 15 to A
+
+  //change value at stack index. This one is kind of weird. "A 15 store 0" would push 15 to A
   if(program[i]=="store" || program[i]=="s") {
     let a = +program[i+1]
     if(a < 0) {
@@ -72,7 +84,8 @@ for(let i = 0; i < program.length; i++) {
     memory.splice(memory.length - 1, 1)
     i = i + 1
   }
-  //load from stack index
+
+  //load from stack index and plce on top of the stack
   if(program[i]=="load" || program[i]=="l") {
     let a = +program[i+1]
     if(a < 0) {
@@ -82,6 +95,7 @@ for(let i = 0; i < program.length; i++) {
     memory.push(memory[a])
     if(debug) console.log("Loaded " + memory[a] + " from index " + a)
   }
+
   //delete stack item at index.
   if(program[i]=="del" || program[i]=="d") {
     let a = +program[i+1]
@@ -92,6 +106,7 @@ for(let i = 0; i < program.length; i++) {
       memory.splice(a, 1)
     if(debug) console.log("deleted item at address " + a)
   }
+
   //print value at memory location
   if(program[i] == "print" || program[i]=="p") {
     let a = +program[i+1]
@@ -102,26 +117,29 @@ for(let i = 0; i < program.length; i++) {
     console.log(memory[a])
     if(debug) console.log("Printed value at index " + a)
   }
+
   //jump to instruction number if the values in the two specified regisers are equal
   if(program[i] == "jump" || program[i]=="j") {
-    //bad workaround to make sure we get the correct memory tag
+    //bad workaround to make sure we get the correct memory label
     let tempCopyOfTag = program[i+1]
     program[i+1] = " "
     let instruction = program.indexOf(tempCopyOfTag)
     program[i+1] = tempCopyOfTag
     let index = +program[i+2]
     let otherIndex = +program[i+3]
+
     if(memory[index] == memory[otherIndex]){
       i = instruction - 1 //one less because i gets incremented at the end of the instruction loop
     if(debug) console.log("jumped to instruction at address " + instruction + ", which is " + program[instruction])
   } else {
     i = i + 3
     if(debug) console.log("Did not jump")
+    }
   }
-  }
+
   //jump to instruction number if the values in the two specified regisers are not equal
   if(program[i] == "notjump" || program[i]=="!j") {
-    //bad workaround to make sure we get the correct memory tag
+    //bad workaround to make sure we get the correct memory label
     let tempCopyOfTag = program[i+1]
     program[i+1] = " "
     let instruction = program.indexOf(tempCopyOfTag)
@@ -136,6 +154,7 @@ for(let i = 0; i < program.length; i++) {
       if(debug) console.log("Did not jump")
     }
   }
+
   //jump to instruction if {instruction, a, b} register a < register b
   if(program[i] == "ltjump" || program[i]=="<") {
     //bad workaround to make sure we get the correct memory label
@@ -154,6 +173,7 @@ for(let i = 0; i < program.length; i++) {
       if(debug) console.log("Did not jump")
     }
   }
+
   //jump to instruction if {instruction, a, b} register a > register b
   if(program[i] == "gtjump" || program[i]==">") {
     //bad workaround to make sure we get the correct memory label
@@ -172,6 +192,7 @@ for(let i = 0; i < program.length; i++) {
       if(debug) console.log("Did not jump")
     }
   }
+
   //pull an element from an array and push it onto the stack
   if(program[i] == "loadelement" || program[i] == "le") {
     let array = program[i + 1]
@@ -180,7 +201,10 @@ for(let i = 0; i < program.length; i++) {
     memory.push(memory[array][index])
     if(debug) console.log("Pulled element from array at index " + array)
   }
-  //pop the last element off of the stack and push it to the index of an array(replace the element if it's already there)
+
+  //pop the last element off of the stack and push it to the index of an array(replace the element if it exists.)
+  //If you give it an index that is out of bounds of the array, it will try to fit it in by adding empty elements.
+  //I have not tested whether or not this works in the negative direction.
   if(program[i] == "storeelement" || program[i] == "se") {
     let array = program[i + 1]
     let index = program[i + 2]
@@ -195,7 +219,7 @@ for(let i = 0; i < program.length; i++) {
   }
 
   if(debug){
-    console.log(memory)
+    console.log(memory) //dump the memory after each instruction
   }
 
 }
